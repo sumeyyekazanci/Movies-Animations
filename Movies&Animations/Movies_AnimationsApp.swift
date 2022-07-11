c245b672e96da1a134d8663c2bae663da9bbe50d
Smyy <sumeyyekazanci13@gmail.com>
2022-07-11 19:52:04 +0300

    Initial Commit

---
+++ Movies&Animations/Movies_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movies_AnimationsApp.swift
+//  Movies&Animations
+//
+//  Created by Sümeyye Kazancı on 11.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Movies_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
